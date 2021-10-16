17f79fdbb6c910d64321f0df66cd2268485a05dc
zengpeiran <peiranzeng66@gmail.com>
2021-10-16 16:45:19 -0500

    initial project

---
+++ src/test/java/com/assessment/notes/NotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
