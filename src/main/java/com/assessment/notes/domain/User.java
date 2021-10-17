82b6401d2ff3cf10485c3020c4bb6f9d7933991b
zengpeiran <peiranzeng66@gmail.com>
2021-10-17 18:04:56 -0500

    add userSecurityService for user authentication

--- src/main/java/com/assessment/notes/domain/User.java
+++ src/main/java/com/assessment/notes/domain/User.java
@@ -1,14 +1,15 @@
 package com.assessment.notes.domain;
 
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Entity
 @Data
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name="id", nullable = false, updatable = false)
@@ -22,4 +23,35 @@ public class User {
     private Date lastUpdateTime;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
     private List<Note> noteList;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<GrantedAuthority> authorites = new HashSet<>();
+        return authorites;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
---
+++ src/main/java/com/assessment/notes/service/impl/UserSecurityService.java
@@ -0,0 +1,23 @@
+package com.assessment.notes.service.impl;
+
+import com.assessment.notes.domain.User;
+import com.assessment.notes.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserSecurityService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+
+        if(user == null) {
+            throw new UsernameNotFoundException("Username not found");
+        }
+
+        return user;
+    }
+}
