baed264861ac4bdd895d629aa09a8c6bc9927df7
zengpeiran <peiranzeng66@gmail.com>
2021-10-17 20:01:34 -0500

    add requestMapping for update user note and create new user note

--- src/main/java/com/assessment/notes/controller/HomeController.java
+++ src/main/java/com/assessment/notes/controller/HomeController.java
@@ -2,19 +2,25 @@ package com.assessment.notes.controller;
 
 import com.assessment.notes.domain.Note;
 import com.assessment.notes.domain.User;
+import com.assessment.notes.service.UserNoteService;
 import com.assessment.notes.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.security.Principal;
+import java.util.Calendar;
 import java.util.List;
 
 @Controller
 public class HomeController {
     @Autowired
     UserService userService;
+    @Autowired
+    UserNoteService userNoteService;
 
     @RequestMapping("/")
     public String index() {return "index";}
@@ -29,6 +35,35 @@ public class HomeController {
         User user = userService.findByEmail(principal.getName());
         model.addAttribute(user);
         List<Note> noteList = userService.findAllUserNote(user);
+        model.addAttribute(noteList);
+
+        return "userNotes";
+    }
+
+    @RequestMapping("/updateNotes")
+    public String updateNotes(@ModelAttribute("id") Long noteId, Model model, @ModelAttribute("newNote")String newNote, Principal principal) {
+        User user = userService.findByEmail(principal.getName());
+        Note note = userNoteService.findById(noteId);
+        note.setNote(newNote);
+        note.setLastUpdateTime(Calendar.getInstance().getTime());
+        userService.updateUserNote(note,user);
+        List<Note> noteList = user.getNoteList();
+        model.addAttribute(noteList);
+
+        return "userNotes";
+    }
+
+    @RequestMapping("/createNotes", method = RequestMethod.POST)
+    public String createNotes(Model model, @ModelAttribute("newNote")String newNote, @ModelAttribute("newNote")String title, Principal principal) {
+        User user = userService.findByEmail(principal.getName());
+        Note note = new Note();
+        note.setCreateTime(Calendar.getInstance().getTime());
+        note.setUser(user);
+        note.setLastUpdateTime(Calendar.getInstance().getTime());
+        note.setTitle(title);
+        note.setNote(newNote);
+        userService.addUserNote(note, user);
+        List<Note> noteList = user.getNoteList();
         model.addAttribute(noteList);
 
         return "userNotes";
